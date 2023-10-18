f8e4c9b4b90760edcaa2e89672a4ac3f8ef7c472
Sourabh0503 <75964371+Sourabh0503@users.noreply.github.com>
2023-10-18 07:02:22 +0000

    modified:   .devcontainer/devcontainer.json 	new file:   basics/datatypes.js 	new file:   basics/variables.js

---
+++ basics/datatypes.js
@@ -0,0 +1,11 @@
+//numbers -> 1 to 2^53
+//bigInt for numbers beyound 2^53 use in trading or big datas like insta facebbok etc
+//string = "sourabh"
+//boolean = true/false
+//undefined -> let x; so here x is undefined
+//null -> standalone value , lets assume a req to sever asking for temp and server is down so it will send null instead to 0 or something which is temp
+//symbol -> unique
+
+// object
+
+console.log(typeof null)
---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+const id=123;  // changes not allowed
+let number=124;  // good to use
+var myName="sourabh"  //there is no scope for var like change can happen anywhere
+city="faridabad" // very bad practice
+
+console.table([id,number,myName,city]);
