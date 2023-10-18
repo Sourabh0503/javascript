e7bb542a98a47ca3eb8f0fa934274419e13a8658
Sourabh0503 <75964371+Sourabh0503@users.noreply.github.com>
2023-10-18 10:12:36 +0000

    start

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let numberX=33;
+console.log(typeof numberX);
+let numberY="33"
+console.log(typeof numberY);
+let numberZ="sourabh"
+console.log(typeof numberZ);
+let numberA=undefined
+console.log(typeof numberA);
+let numberB=null
+console.log(typeof numberB);
+
+let covY=Number(numberY)
+console.log(typeof covY)
+
+//"33"=33
+//33abc=NaN. not a number// but datatyoe of nan is number 
+//null=0
+// true=1/false=0;
+//""=0//to boolean
+//"abc"=1//tp boolean
+
+/********************* Operations **************************/
+let x=3
+let y=-x
+console.log(x);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**3);  //power 
+
+let str1="hello"
+let str2="sourabh"
+console.log(str1+" "+str2);
+
